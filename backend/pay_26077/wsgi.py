fea0ea7066a4d5b174c7b8f1ce60aa4013ab7b32
Crowdbotics <team@crowdbotics.com>
2021-05-03 01:44:00 +0000

    Update models

--- backend/pay_26077/wsgi.py
+++ backend/pay_26077/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pay_26077.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pay_26077.settings")
 
 application = get_wsgi_application()
